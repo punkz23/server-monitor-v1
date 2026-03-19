bfa1ad26567d79b860f906493da8d1cc662c19e4
Francois Ignacio Monilla <igiboi716@gmail.com>
2026-03-19 16:27:01 +0800

    update 3-19-26

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -7,8 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.new_mobile_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36 
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -20,14 +19,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.new_mobile_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 24 
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     buildTypes {
